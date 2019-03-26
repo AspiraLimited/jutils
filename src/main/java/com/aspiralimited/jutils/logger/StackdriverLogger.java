c59b216ad0b337eedbaf5916c4d01bcee40b128b
Petr <petrzhezher@gmail.com>
2019-03-26 17:25:25 +0200

    fixed StackdriverLogger

--- src/main/java/com/aspiralimited/jutils/logger/StackdriverLogger.java
+++ src/main/java/com/aspiralimited/jutils/logger/StackdriverLogger.java
@@ -13,7 +13,7 @@ class StackdriverLogger {
     private static final ObjectMapper MAPPER = new ObjectMapper().disable(FAIL_ON_EMPTY_BEANS);
 
     static void send(Level level, String className, String msg) {
-        print(level, className, msg, null);
+        print(level, className, msg, null, 1);
     }
 
     static void send(Level level, String className, String msg, Object... objects) {
@@ -24,34 +24,37 @@ class StackdriverLogger {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        print(level, className, msg, null);
+        print(level, className, msg, null, 1);
     }
 
     static void send(Level level, String className, String msg, Throwable throwable) {
-        print(level, className, msg, throwable);
+        print(level, className, msg, throwable, 1);
     }
 
     static void send(Level level, String className, Throwable throwable) {
-        print(level, className, throwable.getMessage(), throwable);
+        print(level, className, throwable.getMessage(), throwable, 1);
     }
 
-    private static void print(Level level, String className, String msg, Object object) {
+    private static void print(Level level, String className, String msg, Object object, int retryCount) {
+        String severity = (level == Level.WARN) ? "WARNING" : level.getStandardLevel().toString();
+
+        Map<String, Object> map = Map.of(
+                "severity", severity,
+                "thread", currentThread().getId(),
+                "name", className,
+                "message", msg == null ? "null" : msg,
+                "payload", object == null ? "" : object
+        );
+
         try {
-            String severity = (level == Level.WARN) ? "WARNING" : level.getStandardLevel().toString();
-
-            Map<String, Object> map = Map.of(
-                    "severity", severity,
-                    "thread", currentThread().getId(),
-                    "name", className,
-                    "message", msg == null ? "null" : msg,
-                    "payload", object == null ? "" : object
-
-            );
-
             System.out.println(MAPPER.writeValueAsString(map));
-        } catch (Throwable throwable) {
-            System.out.println(throwable.getMessage());
-            throwable.printStackTrace();
+        } catch (Exception e) {
+            if (retryCount == 1) {
+                print(level, className, msg, object == null ? "" : object.toString(), ++retryCount);
+            } else {
+                System.out.println(e.getMessage());
+                e.printStackTrace();
+            }
         }
     }
 }
